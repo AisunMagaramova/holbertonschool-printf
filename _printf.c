4e6b734cdf33828069150c515baa8653a4b22a89
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-19 14:50:49 +0000

    task 0

--- _printf.c
+++ _printf.c
@@ -8,66 +8,66 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
-    char ch;
-    char *str;
-
-    if (format == NULL)
-        return (-1);
-
-    va_start(args, format);
-
-    while (format[i])
-    {
-        /* eger tek % varsa ve sonra hec ne yoxdursa onda cix */
-        if (format[i] == '%' && format[i + 1] == '\0')
-        {
-            va_end(args);
-            return (-1);
-        }
-
-        if (format[i] == '%' && format[i + 1])
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                ch = va_arg(args, int);
-                write(1, &ch, 1);
-                count++;
-            }
-            else if (format[i] == 's')
-            {
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                while (*str)
-                {
-                    write(1, str++, 1);
-                    count++;
-                }
-            }
-            else if (format[i] == '%')
-            {
-                write(1, "%", 1);
-                count++;
-            }
-            else
-            {
-                /*bilinmeyen format varsa oldugu kimi cap et */
-                write(1, "%", 1);
-                write(1, &format[i], 1);
-                count += 2;
-            }
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        i++;
-    }
-
-    va_end(args);
-    return (count);
+	va_list args;
+	int i = 0, count = 0;
+	char ch;
+	char *str;
+	
+	if (format == NULL)
+		return (-1);
+	
+	va_start(args, format);
+	
+	while (format[i])
+	{
+		/* eger tek % varsa ve sonra hec ne yoxdursa onda cix */
+		if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			va_end(args);
+			return (-1);
+		}
+		
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				ch = va_arg(args, int);
+				write(1, &ch, 1);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					write(1, str++, 1);
+					count++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else
+			{
+				/*bilinmeyen format varsa oldugu kimi cap et */
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+	
+	va_end(args);
+	return (count);
 }
