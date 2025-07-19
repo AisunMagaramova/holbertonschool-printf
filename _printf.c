051a6bd75f5d831a18cf51d8e70f26aa381c47b2
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-19 15:20:15 +0000

    task 0

--- _printf.c
+++ _printf.c
@@ -1,72 +1,85 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+
 /**
- * _printf - formatlı etn çap e
+ * print_char - writes a character to stdout
+ * @args: va_list containing the char argument
+ * Return: number of characters printed (1)
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - writes a string to stdout
+ * @args: va_list containing the string argument
+ * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * _printf - produces output according to a format
  * @format: format string
- * Return: çap olunan simvolların sa
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, count = 0;
-	char ch;
-	char *str;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
-
 	while (format[i])
 	{
-		/* Tek '%' varsa ve sonra heç e yoxdursa çıx */
-		if (format[i] == '%' && format[i + 1] == '\0')
-		{
-			va_end(args);
-			return (-1);
-		}
-
-		if (format[i] == '%' && format[i + 1])
+		if (format[i] == '%')
 		{
 			i++;
 			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
 			{
-				ch = va_arg(args, int);
-				write(1, &ch, 1);
+				write(1, "%", 1);
 				count++;
 			}
-			else if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					write(1, str++, 1);
-					count++;
-				}
-			}
-			else if (format[i] == '%')
-		{
-		write(1, "%", 1);
-		count++;
-		}
+			else if (format[i] == '\0')
+				break; /* Single '%' at end - do nothing */
 			else
-		{
-		write(1, "%", 1);
-	write(1, &format[i], 1);
-	count += 2;
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
 			}
 		}
 		else
 		{
-	write(1, &format[i], 1);
-	count++;
+			write(1, &format[i], 1);
+			count++;
 		}
 		i++;
 	}
-
 	va_end(args);
 	return (count);
 }
