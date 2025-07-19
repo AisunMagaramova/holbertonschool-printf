12bdd1679652701e18fe1b79b09b706ac6907ae2
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-19 14:59:05 +0000

    task 0

--- _printf.c
+++ _printf.c
@@ -10,21 +10,43 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, count = 0;
+	char ch;
+	char *str;
 
-	if (!format)
+	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
 	while (format[i])
 	{
+		/* Tek '%' varsa ve sonra heç e yoxdursa çıx */
+		if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			va_end(args);
+			return (-1);
+		}
+
 		if (format[i] == '%' && format[i + 1])
 		{
 			i++;
 			if (format[i] == 'c')
-				count += print_char(args);
+			{
+				ch = va_arg(args, int);
+				write(1, &ch, 1);
+				count++;
+			}
 			else if (format[i] == 's')
-				count += print_string(args);
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					write(1, str++, 1);
+					count++;
+				}
+			}
 			else if (format[i] == '%')
 			{
 				write(1, "%", 1);
@@ -47,4 +69,4 @@ int _printf(const char *format, ...)
 
 	va_end(args);
 	return (count);
-}	
+}
